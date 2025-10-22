595f855f29e3ea6e619735f988eceb87f114fd2b
Niponpal <niponpal52@gmail.com>
2025-10-22 22:15:42 +0600

    project init🚀🚀

---
+++ src/HRMS.Core/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using HRMS.Core.Entities.BaseEntities;
+
+namespace HRMS.Core.Entities;
+
+public class Employee:AuditableEntity
+{
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public DateTime JoiningDate { get; set; }
+    public decimal SalaryBase { get; set; }
+    public string Status { get; set; } = "Active";
+}
