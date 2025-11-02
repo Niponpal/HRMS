40ef2dd0168b4509954f66b6d178eeb8999d9ae1
Rajdip27 <srajdip920@gmail.com>
2025-11-02 12:24:29 +0600

    add auth 🚀🚑

---
+++ src/HRMS.Infrastructure/Configuration/AuthConfiguration/RoleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static HRMS.Core.Entities.Auth.IdentityModel;
+
+namespace HRMS.Infrastructure.Configuration.AuthConfiguration;
+
+public class RoleConfiguration: IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasData(new Role
+        {
+            Id = 1,
+            Name = "Administrator",
+            NormalizedName = "ADMINISTRATOR",
+
+        }, new Role
+        {
+            Id = 2,
+            Name = "Employee",
+            NormalizedName = "EMPLOYEE",
+        });
+    }
+}
---
+++ src/HRMS.Infrastructure/Configuration/AuthConfiguration/UserRoleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static HRMS.Core.Entities.Auth.IdentityModel;
+
+namespace HRMS.Infrastructure.Configuration.AuthConfiguration;
+
+public class UserRoleConfiguration: IEntityTypeConfiguration<UserRole>
+{
+    public void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.HasData(new UserRole
+        {
+            RoleId = 1,
+            UserId = 1,
+        }, new UserRole
+        {
+            RoleId = 2,
+            UserId = 2,
+        });
+    }
+}
