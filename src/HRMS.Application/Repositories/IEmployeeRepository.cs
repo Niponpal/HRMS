b8a4e54a25cd3b0176400387c05b6f188ab44108
Niponpal <niponpal52@gmail.com>
2025-11-18 11:01:57 +0600

    Fix 🚀🚀🚑 IEmployeeRepository

---
+++ src/HRMS.Application/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using HRMS.Application.CommonModel;
+using HRMS.Application.Filterl;
+using HRMS.Application.ViewModel;
+using Microsoft.AspNetCore.Http;
+
+namespace HRMS.Application.Repositories;
+
+public interface IEmployeeRepository
+{
+    Task<PaginationModel<EmployeeVm>> GetEmployeeAsync(Filter filter, CancellationToken ct);
+    Task<EmployeeVm> GetEmployeeByIdAsync(long id, CancellationToken ct);
+    Task<EmployeeVm> CreateOrUpdateEmployeeAsync(EmployeeVm employeeVm, CancellationToken ct);
+    Task<EmployeeVm> DeleteEmployeeAsync(long id, CancellationToken ct);
+    Task<EmployeeVm> EmployeeUploadExcelAsync(IFormFile file, CancellationToken ct);
+}
